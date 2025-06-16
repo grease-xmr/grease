31da8d2ff9eaf752801f663a200e97fb7e14664a
Cayle Sharrock <CjS77@users.noreply.github.com>
2025-06-16 18:17:53 +0100

    feat: Testnet helper scripts (#63)
    * feat: Testnet helper scripts
    
    Add some scripts to help setup a testnet environment locally.
    
    * review comments

--- README.md
+++ README.md
@@ -17,6 +17,9 @@ that can be implemented in production, but as of now, consider this a proof-of-c
 
 # Using the Grease CLI
 
+(You can run Grease in a local testnet. If you need help setting that up, 
+follow the [testnet setup instructions](./testnet/README.md).)
+
 Both merchant and customer use the same command line interface (CLI) to interact with the Grease network.
 First, you need to create an identity that identifies you on the P2P network.
 
---
+++ testnet/create-wallets.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Create a new wallet for Alice and Bob.
+
+GREASE=grease-cli
+MONEROD=localhost:25070
+
+create_wallet() {
+    local name=$1
+    local config_file="${name}-local.conf"
+    local wallet_file="${name}-local.bin"
+
+    # if the wallet file exists, quit
+    if [ -f "$wallet_file" ]; then
+        echo "Wallet file $wallet_file already exists. Exiting."
+        exit 1
+    fi
+    echo "Creating wallet for $name..."
+    "$GREASE" keypair > "$name-spend-key.txt"
+
+    echo PASTE THE FOLLOWING SPEND KEY AT THE PROMPT WHEN ASKED
+    cat "$name-spend-key.txt"
+
+    monero-wallet-cli --generate-from-spend-key $wallet_file --daemon-address=$MONEROD
+
+    echo "Wallet for $name created successfully."
+    echo ""
+    echo ""
+}
+
+create_wallet alice
+create_wallet bob
